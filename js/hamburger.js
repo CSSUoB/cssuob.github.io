a4c83f56548a264ba4b25b160aa6a9030f068638
Justin Chadwell <jedevc@gmail.com>
2019-06-20 11:17:26 +0100

    Add basic mobile menu

---
+++ js/hamburger.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    console.log('here');
+    let hamburger = document.getElementById('hamburger');
+    let menu = document.getElementById('hamburger-menu');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('visible');
+    });
+
+    menu.addEventListener('click', () => {
+        menu.classList.toggle('visible');
+    });
+};
