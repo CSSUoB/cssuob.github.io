39d4f8591445f3f39b5b45b94067e4a061c2d589
Justin Chadwell <jedevc@gmail.com>
2019-06-20 11:40:21 +0100

    Minor hamburger nice features

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('here');
     let hamburger = document.getElementById('hamburger');
     let menu = document.getElementById('hamburger-menu');
 
@@ -7,7 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('visible');
     });
 
-    menu.addEventListener('click', () => {
-        menu.classList.toggle('visible');
+    menu.addEventListener('click', (event) => {
+        if (event.target == menu) {
+            menu.classList.toggle('visible');
+        }
     });
 });
