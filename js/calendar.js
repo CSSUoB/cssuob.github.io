d0487fa4aba35ca626ae6a8110ffab0436ecf83b
Justin Chadwell <jedevc@gmail.com>
2019-06-21 10:56:09 +0100

    Make pages more easily responsive

--- js/calendar.js
+++ js/calendar.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-	let element = document.getElementById('calendar');
+	let element = document.getElementById('css-calendar');
 	let calendar = new FullCalendar.Calendar(element, {
-		plugins: ['dayGrid', 'googleCalendar'],
-		defaultView: 'dayGridMonth',
+		plugins: ['dayGrid', 'googleCalendar', 'list'],
+		height: 'auto',
+		header: {
+			left: 'prev',
+			center: 'today',
+			right: 'next'
+		},
+		defaultView: window.innerWidth < 800 ? 'listMonth' : 'dayGridMonth',
 		googleCalendarApiKey: 'AIzaSyD-Dmwq4Nf0oSyaPdh_zM2NMqCgYnIRb14',
 		events: {
 			googleCalendarId: 'kg5v9k480jn2qahpmq33h8g7cs@group.calendar.google.com'
