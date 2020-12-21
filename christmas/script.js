2717e43c5b68d027097965a901533cc2f4ebaf9b
Rhysimus <59121865+Rhysimus@users.noreply.github.com>
2020-12-21 16:12:11 +0000

    Indents added
    Easier readability

--- christmas/script.js
+++ christmas/script.js
@@ -1,38 +1,35 @@
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
 $(function(){
-
-if (!$('.envelope').hasClass('open')){
-$('.envelope').click(function(){
-    $(this).removeClass('new').addClass('open');
-    document.body.style.backgroundImage = "url('2.png')";
-});
-}
-
+    if (!$('.envelope').hasClass('open')){
+        $('.envelope').click(function(){
+            $(this).removeClass('new').addClass('open');
+            document.body.style.backgroundImage = "url('2.png')";
+        });
+    }
 });
 
 function resize() {
     if(window.innerWidth / window.innerHeight >= 1.5) //Landscape
     {
-    if(isFirefox)
-    {
-        document.body.style.transform = "scale(" + window.innerHeight / 366.66667 / 1.2 + ")";
-    }
-    else
-    {
-        document.body.style.zoom = (window.innerHeight / 366.66667 / 1.2) * 100 + "%";
-    }
+        if(isFirefox)
+        {
+            document.body.style.transform = "scale(" + window.innerHeight / 366.66667 / 1.2 + ")";
+        }
+        else
+        {
+            document.body.style.zoom = (window.innerHeight / 366.66667 / 1.2) * 100 + "%";
+        }
     }
     else //Portrait
     {
-    document.body.style.zoom = (window.innerWidth / 550 / 1.2) * 100 + "%";
-    if(isFirefox)
-    {
-        document.body.style.transform = "scale(" + window.innerWidth / 550 / 1.2 + ")";
-    }
-    else
-    {
-        document.body.style.zoom = (window.innerWidth / 550 / 1.2) * 100 + "%";
-    }
+        if(isFirefox)
+        {
+            document.body.style.transform = "scale(" + window.innerWidth / 550 / 1.2 + ")";
+        }
+        else
+        {
+            document.body.style.zoom = (window.innerWidth / 550 / 1.2) * 100 + "%";
+        }
     }
 }
