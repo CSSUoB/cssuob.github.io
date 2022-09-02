3215a81364af622f0611238209f13f01a1d1dcec
Justin Chadwell <me@jedevc.com>
2022-09-02 16:56:24 +0100

    Auto-format code using prettier

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let hamburger = document.getElementById('hamburger');
-    let menu = document.getElementById('hamburger-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  let hamburger = document.getElementById("hamburger");
+  let menu = document.getElementById("hamburger-menu");
 
-    hamburger.addEventListener('click', () => {
-        menu.classList.toggle('visible');
-    });
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("visible");
+  });
 
-    menu.addEventListener('click', (event) => {
-        if (event.target == menu) {
-            menu.classList.toggle('visible');
-        }
-    });
+  menu.addEventListener("click", (event) => {
+    if (event.target == menu) {
+      menu.classList.toggle("visible");
+    }
+  });
 });
 
 function toggleShow(id) {
-  let x = document.getElementById("hbdd_"+id)
+  let x = document.getElementById("hbdd_" + id);
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
