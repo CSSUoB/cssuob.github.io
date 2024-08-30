e94f5aed53ddeebefea64b99272828e093a82d49
PhoebeNyx <phoebe-nyx@proton.me>
2024-08-30 20:52:42 +0100

    Add 'p' to allowed tags in calendar event descriptions (fixes #508) (#653)

--- js/calendar.js
+++ js/calendar.js
@@ -294,7 +294,7 @@ function parseHTML(html, otherValidTags = null, otherValidProperties = null) {
     return null;
   }
 
-  const validTags = ["a", "b", "br", "code", "em", "i", "span", "strong", "u"];
+  const validTags = ["a", "b", "br", "code", "em", "i", "p", "span", "strong", "u"];
   const validProperties = new Map([["a", ["href"]]]);
   html = sanitizeTags(
     html,
