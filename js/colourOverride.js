75421afa88b1c48757031e8ee6cbb3684dbff3a9
6C6565 <99056415+qPalee@users.noreply.github.com>
2024-11-18 13:38:24 +0000

    Fix github workflows (#712)
    * Fix Prettier workflow
    
    * Run prettier

--- js/colourOverride.js
+++ js/colourOverride.js
@@ -4,25 +4,25 @@
  *            31st March: Trans Day of Visibility ("tdov")
  *         Month of June: Pride ("pride")
  * 18th December onwards: Christmas ("christmas")
- * 
+ *
  * Classes are defined in:
  *  css/type.scss
  */
 document.addEventListener("DOMContentLoaded", () => {
   let asciiElement = document.getElementsByClassName("ascii")[0];
-  
+
   if (!asciiElement) {
     return;
   }
-    
+
   const currentDate = new Date();
 
-  // 31st March 
+  // 31st March
   if (currentDate.getMonth() === 2 && currentDate.getDate() === 31) {
     asciiElement.classList.add("tdov");
   }
 
-  // June 
+  // June
   if (currentDate.getMonth() === 5) {
     asciiElement.classList.add("pride");
   }
